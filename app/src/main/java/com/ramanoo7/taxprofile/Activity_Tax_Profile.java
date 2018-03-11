d935f7ac7e8fc666a21329ccef7dd43ec090aabe
Raman <37237210+ramanOO7@users.noreply.github.com>
2018-03-11 23:18:48 +0530

    2nd Page

---
+++ app/src/main/java/com/ramanoo7/taxprofile/Activity_Tax_Profile.java
@@ -0,0 +1,13 @@
+package com.ramanoo7.taxprofile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity_Tax_Profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tax_profile);
+    }
+}
